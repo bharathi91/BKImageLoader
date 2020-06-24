9926fe3f8853dab04a084fe4854fc0cded2892ec
Bharathi Kumar <bharathi91m@gmail.com>
2020-06-24 11:07:49 +0530

    xcode project added

---
+++ BKImageLoader/BKImageLoader.h
@@ -0,0 +1,12 @@
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BKImageLoader.
+FOUNDATION_EXPORT double BKImageLoaderVersionNumber;
+
+//! Project version string for BKImageLoader.
+FOUNDATION_EXPORT const unsigned char BKImageLoaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BKImageLoader/PublicHeader.h>
+
+
